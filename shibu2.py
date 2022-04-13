b40eb2da7b95d7d516c56648b627f2fad17b3f80
shezin888 <shezins888@gmail.com>
2022-04-13 16:38:34 +0530

    added text to speech

---
+++ shibu2.py
@@ -0,0 +1,17 @@
+
+from gtts import gTTS
+
+
+import os
+def t2s (text):
+
+    mytext = text
+
+    language = 'en'
+
+
+    myobj = gTTS(text=mytext, lang=language, slow=False)
+    myobj.save("welcome.mp3")
+    os.system("start welcome.mp3")
+
+t2s("")
